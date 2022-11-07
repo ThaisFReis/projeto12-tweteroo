f864f8b8f0e660bb905f2520fdbe33cc75022a0a
ThaisFReis <reis.thaisf@gmail.com>
2022-11-07 03:33:16 -0300

    Post do sing-up terminado

--- index.js
+++ index.js
@@ -8,9 +8,33 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+const users = [];
+const tweets = [];
 
 
+app.post("/sing-up", (req, res) => {
+    const user = req.body;
+
+    if (user.username === "" || user.avatar === "") {
+        return res.status(400).send("Todos os campos são obrigatórios");
+    }
+
+    users.push(user);
+    res.status(201).send(user);
+    }
+)
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    
+    }
+)
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
